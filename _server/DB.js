e80e2fab22c9b4ec0847b46a0d631d9b2ab7d323
Joquori <92768969+Joquori1@users.noreply.github.com>
2022-07-11 17:13:46 -0500

    Delete _server directory

--- _server/DB.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DB: 'mongodb+srv://hangman:hangman1@cluster0-hgmn.qikzqqo.mongodb.net/db-hangman?retryWrites=true&w=majority'
-}
--- _server/app.js
+++
@@ -1,7 +0,0 @@
-var app = require("./index");
-const PORT = 3001;
-
-
-app.listen(PORT, () => {
-    console.log(`SERVER RUNNING SUCCESSFULLY! @ ${PORT}`);
-});
